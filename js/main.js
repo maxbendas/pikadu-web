d4b4d21dcd79687a79f868937d327bce92baaa70
maxbendas <56072735+maxbendas@users.noreply.github.com>
2020-11-12 23:17:24 +0200

    day 3.1

--- js/main.js
+++ js/main.js
@@ -24,13 +24,16 @@ const editPhotoURL = document.querySelector('.edit-photo');
 const userAvatarElem = document.querySelector('.user-avatar');
 
 const postsWrapper = document.querySelector('.posts')
+const buttonNewPost = document.querySelector('.button-new-post')
+const addPostElem = document.querySelector('.add-post')
 
 const listUsers = [
     {
         id: '01',
         email: 'maks@mail.com',
         password: '12345',
-        displayName: 'MaksJS'
+        displayName: 'MaksJS',
+        photo: 'https://wl-adme.cf.tsp.li/resize/728x/jpg/cf3/3eb/cebf385e9da99914a27f31c22d.jpg'
     },
     {
         id: '02',
@@ -111,7 +114,10 @@ const setPosts = {
             title: 'Заголовок поста',
             text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
             tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
-            author: 'maks@mail.com',
+            author: {
+                displayName: 'maks',
+                photo: 'https://wl-adme.cf.tsp.li/resize/728x/jpg/cf3/3eb/cebf385e9da99914a27f31c22d.jpg'
+            },
             date: '11.11.2020, 20:54:00',
             like: 15,
             comments: 20,
@@ -120,12 +126,44 @@ const setPosts = {
             title: 'Заголовок поста2',
             text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam blanditiis ipsum, mollitia neque perspiciatis praesentium tempore. Accusantium blanditiis dolorum earum facere necessitatibus quam quisquam totam? Aspernatur atque dicta dolores inventore laboriosam maxime officia, omnis quam veritatis? Eos nostrum, odit repellat similique sint totam voluptate! Deleniti dolores eaque iure pariatur quae quam reiciendis, voluptatem! Dolorum eaque neque odit quisquam quos.',
             tags: ['свежее', 'новое', 'мое', 'случайность'],
-            author: 'kate@mail.com',
+            author: {
+                displayName: 'kate',
+                photo: 'https://cdn22.img.ria.ru/images/07e4/06/08/1572654825_0:17:640:377_600x0_80_0_0_87b65178b7d57533d463aa962b2a2091.jpg'
+            },
             date: '10.11.2020, 20:54:00',
             like: 25,
             comments: 30,
+        },
+        {
+            title: 'Заголовок поста',
+            text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+            tags: ['свежее', 'новое', 'горячее', 'мое', 'случайность'],
+            author: {
+                displayName: 'maks',
+                photo: 'https://wl-adme.cf.tsp.li/resize/728x/jpg/cf3/3eb/cebf385e9da99914a27f31c22d.jpg'
+            },
+            date: '11.11.2020, 20:54:00',
+            like: 15,
+            comments: 20,
+        },
+    ],
+    addPost(title, text, tags, handler){
+        this.allPosts.unshift({
+            title,
+            text,
+            tags: tags.split(',').map(item=>item.trim()),
+            author:{
+                displayName:setUsers.user.displayName,
+                photo:setUsers.user.photo,
+            },
+            date:new Date().toLocaleString(),
+            like:0,
+            comments:0,
+        })
+        if (handler){
+            handler();
         }
-    ]
+    }
 }
 
 const toggleAuthDom = () => {
@@ -136,25 +174,38 @@ const toggleAuthDom = () => {
         loginElem.style.display = 'none';
         userElem.style.display = '';
         userNameElem.textContent = user.displayName;
-        userAvatarElem.src = user.photo || userAvatarElem.src
+        userAvatarElem.src = user.photo || userAvatarElem.src;
+        buttonNewPost.classList.add('visible')
     } else {
         loginElem.style.display = '';
         userElem.style.display = 'none';
+        buttonNewPost.classList.remove('visible');
+        addPostElem.classList.remove('visible')
+        postsWrapper.classList.add('visible')
+        //    TODO
+        addPostElem.classList.add('visible')
+        postsWrapper.classList.remove('visible')
+        //    end
     }
+}
+
+const showAddPost = () => {
+    addPostElem.classList.add('visible')
+    postsWrapper.classList.remove('visible')
 }
 
 const showAllPosts = () => {
 
     let postsHTML = '';
 
-    setPosts.allPosts.forEach(({title, text, date, like, comments, tags, author})=>{
+    setPosts.allPosts.forEach(({title, text, date, like, comments, tags, author}) => {
         postsHTML += `
                 <section class="post">
             <div class="post-body">
                 <h2 class="post-title">${title}</h2>
                 <p class="post-text">${text}</p>
                 <div class="tags">
-                ${tags.map((tag)=>`<a href="#" class="tag">#${tag}</a>`).join('')}                    
+                ${tags.map((tag) => `<a href="#" class="tag">#${tag}</a>`).join('')}                    
                 </div>
             </div>
             <div class="post-footer">
@@ -184,10 +235,10 @@ const showAllPosts = () => {
                 </div>
                 <div class="post-author">
                     <div class="author-about">
-                        <a href="#" class="author-username">${author}</a>
+                        <a href="#" class="author-username">${author.displayName}</a>
                         <span class="post-time">${date}</span>
                     </div>
-                    <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+                    <a href="#" class="author-link"><img src=${author.photo || "img/avatar.jpeg"} alt="avatar" class="author-avatar"></a>
                 </div>
             </div>
         </section>
@@ -195,7 +246,10 @@ const showAllPosts = () => {
     })
 
     postsWrapper.innerHTML = postsHTML
-        // 'Тут могла быть ваша реклама!'
+    // 'Тут могла быть ваша реклама!'
+
+    addPostElem.classList.remove('visible')
+    postsWrapper.classList.add('visible')
 }
 
 const init = () => {
@@ -236,6 +290,29 @@ const init = () => {
         // вешаем класс на меню, когда кликнули по кнопке меню
         menu.classList.toggle('visible');
     })
+
+    buttonNewPost.addEventListener('click', event => {
+        event.preventDefault();
+        showAddPost()
+    })
+
+    addPostElem.addEventListener('submit', event => {
+            event.preventDefault();
+            const {title, text, tags} = addPostElem.elements;
+            if (title.value.length < 6) {
+                alert('Слишком короткий заголовок');
+                return;
+            }
+            if (text.value.length < 50) {
+                alert('Слишком короткий текст');
+                return;
+            }
+            setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
+            addPostElem.classList.remove('visible');
+        addPostElem.reset()
+
+        }
+    )
 
     showAllPosts()
     toggleAuthDom()
